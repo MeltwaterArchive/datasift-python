289d9f9cd85b8d030cb9d60480fb0e5b4646aac5
Jacek Artymiak <jacek.artymiak@datasift.com>
2012-11-23 14:51:10 +0000

    Fixed path issues in make-docs.sh.

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -1,8 +1,11 @@
 mkdir ../../master/datasift/doc
+mkdir ../../master/datasift/doc/_static
 cp Makefile ../../master/datasift/doc
 cp conf.py ../../master/datasift/doc
 cp index.rst ../../master/datasift/doc
-sphinx-build -b html datasift-python gh-pages
 cd ../../master/datasift/doc
-cd ../../gh-pages/doc-tools
-cp -a ../master/datasift/doc/_build/html ./..
+export PYTHONPATH=$PYTHONPATH:../..
+make html
+#sphinx-build -b html ../../datasift-python gh-pages
+cd ../../../gh-pages/doc-tools
+cp -a ../../master/datasift/doc/_build/html/* ./..
