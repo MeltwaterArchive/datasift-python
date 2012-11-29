3cb413f8cbf8f4af12e65f548610210bacefbb7d
Jason Dugdale <jason.dugdale@gmail.com>
2012-11-29 10:28:48 +0000

    Bumped to v.0.5.2

--- datasift/__init__.py
+++ datasift/__init__.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 __author__  = "Stuart Dallas <stuart@3ft9.com>"
 __status__  = "beta"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __date__    = "30 August 2012"
 
 #-----------------------------------------------------------------------------
