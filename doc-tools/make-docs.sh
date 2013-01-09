d8b5f58e865862ad76643223ee5ebd2945eddb11
Jacek Artymiak <jacek.artymiak@datasift.com>
2013-01-09 10:58:41 +0000

    Changed working directory to /tmp. Fixed cp stat errror issue.

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #-v
 
-export BASE_DIR="$( cd "$( dirname $0 )/../../../.." && pwd )/"
+export BASE_DIR="`pwd`/"
 
 source ${BASE_DIR}ms-tools/doc-tools/docathon/sub/make-docs-util-defs.sh
+export BASE_DIR="/tmp/$(basename $0).$$.tmp/"
 initialise $*
 
 ### Python-specific parameters
@@ -36,7 +37,7 @@ cp -v ${GH_PAGES_DIR}doc-tools/index.rst ${CODE_DIR}datasift/doc ; stop_on_error
 (
 	message "copying documents"
 	cd ${GH_PAGES_DIR} ; stop_on_error
-	cp -a "${CODE_DIR}datasift/doc/_build/html/*" . ; stop_on_error
+	cp -a ${CODE_DIR}datasift/doc/_build/html/* . ; stop_on_error
 ) || error "stopped parent"
 
 (
