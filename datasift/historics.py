71681f34e1a3cec96341394b6bc06f7efc0219bb
Andi Miller <andi.miller@datasift.com>
2014-01-21 16:31:37 +0000

    Added pydoc for historics

--- datasift/historics.py
+++ datasift/historics.py
@@ -2,7 +2,8 @@ from datasift.exceptions import HistoricSourcesRequired
 
 
 class Historics(object):
-    """Class which represents the DataSift Historics REST API and provides the ability to query it."""
+    """ Represents the DataSift Historics REST API and provides the ability to query it.
+        Internal class instantiated as part of the Client object. """
     def __init__(self, request):
         self.request = request.with_prefix('historics')
 
@@ -21,6 +22,7 @@ class Historics(object):
             :type sources: list
             :param sample: percentage to sample, either 10 or 100
             :type sample: int
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
         """
         if len(sources) == 0:
             raise HistoricSourcesRequired()
@@ -37,34 +39,95 @@ class Historics(object):
             :type historics_id: str
             :return: dict of REST API output with headers attached
             :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
         """
         return self.request.post('start', data=dict(id=historics_id))
 
     def update(self, historics_id, name):
-        """Update the name of the given Historics query."""
+        """ Update the name of the given Historics query.
+
+            :param historics_id: playback id of the job to start
+            :type historics_id: str
+            :param name: new name of the stream
+            :type name: str
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         return self.request.post('update', data=dict(id=historics_id, name=name))
 
     def stop(self, historics_id, reason=''):
-        """Stop the historics with the given ID."""
+        """ Stop an existing Historics query.
+
+            :param historics_id: playback id of the job to stop
+            :type historics_id: str
+            :param reason: optional reason for stopping the job
+            :type reason: str
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         return self.request.post('stop', data=dict(id=historics_id, reason=reason))
 
     def status(self, start, end, sources=None):
-        """Check the data coverage in the Historics archive for a given interval."""
+        """ Check the data coverage in the Historics archive for a given interval.
+
+            :param start: Unix timestamp for the start time
+            :type start: int
+            :param end: Unix timestamp for the start time
+            :type end: int
+            :param sources: list of data sources to include.
+            :type sources: list
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         params = {'start': start, 'end': end}
         if sources:
             params['sources'] = ','.join(sources)
         return self.request.get('status', params=params)
 
     def delete(self, historics_id):
-        """Delete the historics with the given ID."""
+        """ Delete one specified playback query. If the query is currently running, stop it.
+
+            status_code is set to 204 on success
+
+            :param historics_id: playback id of the query to delete
+            :type historics_id: str
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         return self.request.post('delete', data=dict(id=historics_id))
 
     def get_for(self, historics_id, with_estimate=None):
-        """Get the historic query for the given ID"""
+        """ Get the historic query for the given ID
+
+            :param historics_id: playback id of the query
+            :type historics_id: str
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         return self.get(historics_id, maximum=None, page=None, with_estimate=with_estimate)
 
     def get(self, historics_id=None, maximum=None, page=None, with_estimate=None):
-        """Get the historics with the given ID, If no ID is provided then get a list of Historic queries."""
+        """ Get the historics query with the given ID, if no ID is provided then get a list of historics queries.
+
+            :param historics_id: (optional) ID of the query to retrieve
+            :type historics_id: str
+            :param maximum: (optional) maximum number of queries to recieve (default 20)
+            :type maximum: int
+            :param page: (optional) page to retrieve for paginated queries
+            :type page: int
+            :param with_estimate: include estimate of completion time in output
+            :type with_estimate: bool
+            :param historics_id: playback id of the query
+            :type historics_id: str
+            :return: dict of REST API output with headers attached
+            :rtype: request.Response
+            :raises: DataSiftApiException, requests.exceptions.HTTPError
+        """
         params = {'id': historics_id}
         if maximum:
             params['max'] = maximum
