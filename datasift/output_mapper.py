45aeb3fb94e07b1bdafd687f8e140d28f0d62527
Andi Miller <andi.miller@datasift.com>
2014-02-04 11:45:32 +0000

    Added comments to, and refactored the output mapper

--- datasift/output_mapper.py
+++ datasift/output_mapper.py
@@ -5,21 +5,21 @@ import six
 
 
 def date_handler_long(d, prefix, endpoint):
-    if prefix == "historics" and isinstance(d, six.string_types) and not (" " in d):
+    if prefix == "historics" and isinstance(d, six.string_types) and not (" " in d):  # historics sometimes returns string encoded unix timestamps
             d = int(d)
     if d is None:
         return None  # special case for end=None coming out of push
     if isinstance(d, six.string_types):
-        return datetime.strptime(d, "%a, %d %b %Y %H:%M:%S +0000")
+        return datetime.strptime(d, "%a, %d %b %Y %H:%M:%S +0000")  # rfc2822 email dates
     else:
-        return datetime.fromtimestamp(d)
+        return datetime.fromtimestamp(d)  # standard UNIX timestamp
 
 
 def date_handler_short(d, prefix, endpoint):
     if isinstance(d, six.string_types):
-        return datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
+        return datetime.strptime(d, "%Y-%m-%d %H:%M:%S")  # short datetime with no timezone data
     elif isinstance(d, int):
-        return datetime.fromtimestamp(d)
+        return datetime.fromtimestamp(d)  # standard UNIX timestamp
 
 
 def float_handler(d, p, e):
@@ -35,13 +35,24 @@ output_map = {
 
 
 def outputmapper(data, prefix, endpoint):
+    """ Internal function used to traverse a data structure and map the contents onto python-friendly objects inplace.
+
+        This uses recursion, so try not to pass in anything that's over 255 objects deep.
+
+        :param data: data structure
+        :type data: any
+        :param prefix: endpoint family, eg. sources, historics
+        :type prefix: str
+        :param endpoint: endpoint being called on the API
+        :type endpoint: str
+        :returns: Nothing, edits inplace
+    """
     if isinstance(data, list):
         for item in data:
             outputmapper(item, prefix, endpoint)
-    if isinstance(data, dict):
+    elif isinstance(data, dict):
         for map_target in output_map:
             if map_target in data:
                 data[map_target] = output_map[map_target](data[map_target], prefix, endpoint)
         for item in data.values():
-            if isinstance(data, (dict, list)):
-                outputmapper(item, prefix, endpoint)
+            outputmapper(item, prefix, endpoint)
