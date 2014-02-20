72ccb02809211b2ffcd4ffd7c6c22bca7193ac56
Andi Miller <andi.miller@datasift.com>
2014-02-20 12:25:08 +0000

    bumped setup.py to 2.0.2 for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os.path
 
 setup(
     name="datasift-beta",
-    version="2.0.1",
+    version="2.0.2",
     author="DataSift",
     author_email="opensource@datasift.com",
     maintainer="DataSift",
