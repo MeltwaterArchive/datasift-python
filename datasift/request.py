b86be8d2e79a96753fae049ded0b7253c18904ed
Andi Miller <andi.miller@datasift.com>
2014-02-21 12:28:11 +0000

    Made DataSiftApiFailure a bit more verbose

--- datasift/request.py
+++ datasift/request.py
@@ -76,7 +76,7 @@ class PartialRequest(object):
             try:
                 data = parser(response.text)
             except ValueError as e:
-                raise DataSiftApiFailure("Unable to decode returned data.")
+                raise DataSiftApiFailure("Unable to decode returned data. %s" % e)
             if "error" in data:
                 if response.status_code == 401:
                     raise AuthException(data)
