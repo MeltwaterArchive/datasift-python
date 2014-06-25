47bf710ffff860c2c4f24f3c43c8171e2d7ca817
Andi Miller <andi.miller@datasift.com>
2014-06-25 16:08:52 +0100

    fixed livestreaming with multiple subscriptions

--- datasift/live_stream.py
+++ datasift/live_stream.py
@@ -1,13 +1,30 @@
 from __future__ import print_function
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
 from twisted.internet.protocol import ReconnectingClientFactory
+from twisted.internet.task import LoopingCall
 
 
 class LiveStream(WebSocketClientProtocol):  # pragma: no cover
     """ Internal class used to call the websocket callbacks. """
 
+    sendqueue = []
+    sending = False
+
+    def _sender(self):
+        if len(self.sendqueue):
+            while self.sendqueue:
+                data = self.sendqueue.pop()
+                self.sendMessage(data)
+
+    def queueMessage(self, message):
+        self.sendqueue.append(message)
+        if not self.sending:
+            self.sending = True
+            self._loopingcall = LoopingCall(self._sender)
+            self._loopingcall.start(0.1)
+
     def onOpen(self):
-        self.factory.datasift['send_message'] = self.sendMessage
+        self.factory.datasift['send_message'] = self.queueMessage
         self.factory.datasift['on_open']()
 
     def onClose(self, wasClean, code, reason):
