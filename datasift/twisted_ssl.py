6ba0e53a2dc61c21488dac2cc447da34ef859289
Andi Miller <andi.miller@datasift.com>
2015-02-11 16:29:25 +0000

    Upgrade Twisted SSL adapter to TLS 1.0+

--- datasift/twisted_ssl.py
+++ datasift/twisted_ssl.py
@@ -5,14 +5,5 @@ class ClientContextFactory:
 
         isClient = 1
 
-        # SSLv23_METHOD allows SSLv2, SSLv3, and TLSv1.  We disable SSLv2 and SSLv3 below,
-        # though.
-        method = SSL.SSLv3_METHOD
-
-        _contextFactory = SSL.Context
-
         def getContext(self):
-                ctx = self._contextFactory(self.method)
-                ctx.set_options(SSL.OP_NO_SSLv3)
-                ctx.set_options(SSL.OP_NO_SSLv2)
-                return ctx
+		return SSL.Context(SSL.TLSv1_METHOD)
