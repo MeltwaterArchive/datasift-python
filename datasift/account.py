f0a2d9e1b36f05b9fc4a7c20d49b7a32df060316
Andi Miller <andi.miller@datasift.com>
2015-05-21 17:52:13 +0100

    pep8

--- datasift/account.py
+++ datasift/account.py
@@ -1,9 +1,6 @@
-
-
 class Account(object):
     """ Represents the DataSift account REST API and provides the ability to query it.
         Internal class instantiated as part of the Client object. """
 
     def __init__(self, request):
-        self.request  = request.with_prefix('account')
-
+        self.request = request.with_prefix('account')
